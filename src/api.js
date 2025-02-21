ee9d3e6c32eef50f3a930e84ab2492a527648373
rlarldud1234 <rlarldud6482@naver.com>
2025-02-21 10:18:16 +0900

    create : axios를 활용한 api 연동 코드 작성

---
+++ src/api.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+const getRequest = () => {
+  const request = axios.create({
+    timeout: 10000,
+    baseURL: "https://api-v2.adrop.io",
+    headers: {
+      Authorization:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6IjAxSEQ1UTNaOEdYTk5XRjQ2NENOV0NNQlM0OjAxSEZCTU1HRkZXUU5OMk5aVkpCNU5GUFZaIiwia2V5VHlwZSI6MSwiaWF0IjoxNzAwMTI0MDQ5LCJleHAiOjI1MzQwMjEyODAwMH0.J877rLr4xvUWqcEzsesEp_KNzO8XnHaJeESPXdABubQ",
+    },
+  });
+
+  return request;
+};
+
+export const fetchAd = async () => {
+  const params = {
+    unit: "PUBLIC_TEST_UNIT_ID_375_80",
+    uid: "asdasdasd",
+    pf: "web",
+    lcl: "ko_	KR",
+  };
+
+  const response = await getRequest().get("/request", { params });
+
+  return response.data;
+};
